c351bd2f231d7649265475f853999781fa583771
leuu-1998 <lmoinah@uni.pe>
2022-01-15 20:10:59 -0500

    create Dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <div>Hello Wolrd!</div> <Dashboard />
+    </h1>
+  );
 }
 
 export default App;
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <nav>
+        <div>simulacro</div>
+      </nav>
+    </div>
+  );
+}
