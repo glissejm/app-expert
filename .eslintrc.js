f040436a12c7b6e8cd4a6be95d0fed04350509aa
GlisseLisbeth <glisselisbeth@gmail.com>
2022-01-15 21:09:15 -0500

    configuration of prettier, eslint and husky

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "react-app",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    noConsole: "off",
+    noUndef: "error",
+    preferTemplate: "error",
+    noConsole: "warn",
+  },
+};
