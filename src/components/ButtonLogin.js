7ca338976139f38330af3d1f6a4c364905bf8d1c
Omar <omarftt@gmail.com>
2022-01-23 18:11:24 -0500

    cherry-pick login/register

---
+++ src/components/ButtonLogin.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function ButtonLogin({ name, buttonStyle }) {
+  return (
+    <button type="submit" class="w-full block bg-blueThird hover:bg-darkSecondary focus:bg-blueThird font-semibold rounded-lg
+    px-4 py-2 mt-4 border border-dark">{name}</button>
+  );
+}
