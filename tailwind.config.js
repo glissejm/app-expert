a7bcc7eca8fd8b72b7799dc408734d4c2a795593
glissejm <91516085+glissejm@users.noreply.github.com>
2022-01-23 12:52:39 -0500

    update home (#14)
    Co-authored-by: EparionaD <eparionad@gmail.com>
    Co-authored-by: GlisseLisbeth <glisselisbeth@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#08415C",
+        darkPrimary: "#072227",
+        secundary: "#EDF2EF",
+        lightSecondary: "#AEFEFF",
+        darkSecondary: "#4FBDBA",
+        brightOrange: "#FFB800",
+        brightRed: "#DE3C4B",
+      },
+    },
   },
   plugins: [],
 };
