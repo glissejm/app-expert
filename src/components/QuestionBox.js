cbcfbf1017fbe017bed8d89a91971dddf4ebd275
leuu-1998 <lmoinah@uni.pe>
2022-01-24 00:45:26 -0500

    cherry pick dashboard refactoring

---
+++ src/components/QuestionBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function QuestionBox({ id, name, topic, difficult }) {
+  return (
+    <button
+      className="rounded-full flex items-center place-content-around  bg-[#072227]"
+      id="question"
+      type="button"
+      key={id}
+    >
+      <h3 className="font-bold text-[#AEFEFF]">{name}</h3>
+      <p className="text-white">{topic}</p>
+      <p className="text-white">{difficult}</p>
+    </button>
+  );
+}
