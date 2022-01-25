7eaa07de209ac32fa2669659db21ea25ea34c6c9
glissejm <91516085+glissejm@users.noreply.github.com>
2022-01-25 09:02:49 -0500

    add login (#22)
    Co-authored-by: GlisseLisbeth <glisselisbeth@gmail.com>

--- src/components/ButtonLogin.js
+++ src/components/ButtonLogin.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-export default function ButtonLogin({ name, buttonStyle }) {
+export default function ButtonLogin({ name, buttonStyle, handleLogin }) {
   return (
-    <button type="submit" class="w-full block bg-blueThird hover:bg-darkSecondary focus:bg-blueThird font-semibold rounded-lg
-    px-4 py-2 mt-4 border border-dark">{name}</button>
+    <button
+      type='submit'
+      class='w-full block bg-blueThird hover:bg-darkSecondary focus:bg-blueThird font-semibold rounded-lg
+    px-4 py-2 mt-4 border border-dark'
+      onClick={handleLogin}
+    >
+      {name}
+    </button>
   );
 }
--- src/components/FormLogin/FormLogin.js
+++ src/components/FormLogin/FormLogin.js
@@ -1,39 +1,62 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/authContext';
+import { types } from '../../types/types';
 import ButtonLogin from './../ButtonLogin';
 import InputLogin from './../InputLogin';
 
 export default function FormLogin() {
+  const navigate = useNavigate();
+  const { dispatch } = useContext(AuthContext);
+
+  const handleLogin = () => {
+    const action = {
+      type: types.login,
+      payload: { name: 'Glisse' },
+    };
+
+    dispatch(action);
+
+    const lastPath = localStorage.getItem('lastPath') || '/dashboard';
+
+    navigate(lastPath, {
+      replace: true,
+    });
+  };
+
   return (
-        
-    <form class="mt-6" action="#" methos="POST">
-        
-        <div>
-            <InputLogin type="email" name="Ingresar Usuario" />
+    <form className='mt-6' action='#' methos='POST'>
+      <div>
+        <InputLogin type='email' name='Ingresar Usuario' />
+      </div>
+      <div className='mt-4'>
+        <InputLogin type='password' name='Ingresar Contraseña' />
+      </div>
+
+      <div className='mb-8 form-check'>
+        <input
+          type='checkbox'
+          name='connected'
+          className='form-check-input mt-3'
+        />
+        <label for='connected' className='form-check-label mx-2'>
+          Mantenerme conectado
+        </label>
+      </div>
+
+      <ButtonLogin name='Iniciar Sesión' />
+
+      <hr className='my-6 border-gray-300 w-full' />
+
+      <button
+        type='button'
+        className='w-full block bg-blueSecondary hover:bg-white focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-2 border border-gray-300'
+      >
+        <div className='flex items-center justify-center'>
+          <span className='ml-4'>Iniciar sesion con Google</span>
         </div>
-        <div class="mt-4">
-            <InputLogin type="password" name="Ingresar Contraseña" />    
-        </div>
-
-        <div class="mb-8 form-check">
-            <input type="checkbox" 
-                    name="connected" 
-                    class="form-check-input mt-3" />
-            <label for="connected" class="form-check-label mx-2">Mantenerme conectado</label>
-        </div>
-
-        <ButtonLogin name="Iniciar Sesión" />
-
-        <hr class="my-6 border-gray-300 w-full" />
-
-        <button type="button" class="w-full block bg-blueSecondary hover:bg-white focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-2 border border-gray-300">
-            <div class="flex items-center justify-center">
-                <span class="ml-4">
-                Iniciar sesion con Google
-                </span>
-            </div>
-        </button>
-        <ButtonLogin name="Registrarse" />
-        
+      </button>
+      <ButtonLogin name='Registrarse' handleLogin={handleLogin} />
     </form>
   );
 }
