e6a1a1b8ee5d43140d31d28d22b78d2e28a942c6
Glisse <glisse@Glisses-MacBook-Pro.local>
2022-03-10 20:29:34 -0500

    Resize-flex questionId

--- src/components/CoursesDetail.jsx
+++ src/components/CoursesDetail.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-export default function CoursesDetail({title,content,windowStyle,contentStyle}) {
-  return(
-      <article className={`drop-shadow-md border-2 bg-slate-200 border-third rounded-lg ${windowStyle}`}>
-          <div className='bg-third px-3 py-2 text-secondary font-bold'>
-              <h3>{title}</h3>
-          </div>
-          <div className={`px-3 py-2  ${contentStyle}`}>
-            <p>{content}</p>
-          </div>
-      </article>
+export default function CoursesDetail({ title, content, contentStyle }) {
+  return (
+    <>
+      <div className='bg-third px-3 py-2 text-secondary font-bold'>
+        <h3>{title}</h3>
+      </div>
+      <div className={`px-3 py-2  ${contentStyle}`}>
+        <p>{content}</p>
+      </div></>
   )
 }
