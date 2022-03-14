4469553b9e294d1d2179ce65476a38e838683a0f
Ernesto Pariona Díaz <eparionad@gmail.com>
2022-03-14 16:55:54 -0500

    add component profile

---
+++ src/pages/Landing/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarClient from '../../components/NavbarClient/NavbarClient';
+import ProfileUser from '../../components/ProfileUser';
+
+const Profile = () => {
+  return (
+    <>
+      <NavbarClient />
+      <ProfileUser />
+    </>
+  );
+};
+
+export default Profile;
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
-import Exercise from "../pages/Exercise/Exercise";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Exercise from '../pages/Exercise/Exercise';
+import Profile from '../pages/Landing/Profile';
 
 export default function DashboardRoutes() {
   return (
     <Routes>
-      <Route path="dashboard" element={<Dashboard />} />
-      <Route path="simulacro" element={<Exercise />} />
-      <Route path="dashboard/:id" element={<Exercise />} />
+      <Route path='dashboard' element={<Dashboard />} />
+      <Route path='simulacro' element={<Exercise />} />
+      <Route path='dashboard/:id' element={<Exercise />} />
+      <Route path='perfil' element={<Profile />} />
     </Routes>
   );
 }
