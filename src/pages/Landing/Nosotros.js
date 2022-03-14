55dcbcf71ce6c9a382c46add279bc0f4ad8a4c71
Ernesto Pariona Díaz <eparionad@gmail.com>
2022-03-14 17:34:14 -0500

    add component about us

--- src/pages/Landing/Nosotros.js
+++ src/pages/Landing/Nosotros.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import About from '../../components/About';
 
 export default function Nosotros() {
   return (
     <>
       <Navbar />
-      <h1>Este es la página de Nosotros</h1>
+      <About />
     </>
   );
 }
